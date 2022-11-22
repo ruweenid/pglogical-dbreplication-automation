bafe824b892016053dd9fe8f36d98326507b90d7
ruweenid <ruween.iddagoda@velaris.io>
2022-11-22 12:00:07 +0530

    bug fixes: subscription password issue

--- create_node.sh
+++ create_node.sh
@@ -9,7 +9,7 @@ done
 
 function create_node(){
     PGPASSWORD=$database_password psql -h $database_hostname -p $database_port -U $database_user -d $database_name << EOF
-        SELECT pglogical.create_node( node_name := '$node_name', dsn := 'host=$database_hostname port=5432 dbname=$database_name user=pguser password=$database_password');
+        SELECT pglogical.create_node( node_name := '$node_name', dsn := 'host=$database_hostname port=5432 dbname=$database_name user=pguser password=$pglogical_password');
 EOF
 }
 
